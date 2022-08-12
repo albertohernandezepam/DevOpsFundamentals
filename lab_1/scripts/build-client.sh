16dc6871333ee0189bd199921ca35a978d482c8a
José Alberto Hernández Sánchez <alberto_hernandez@epam.com>
2022-08-12 15:46:42 -0500

    Se ha agregado parcialmente el paso tres, faltó automatizar el audit,etc

--- lab_1/scripts/build-client.sh
+++ lab_1/scripts/build-client.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "$( git --version )"
+clear
+cd ../../../clientRepo/shop-angular-cloudfront
+npm install
+if [[ -e  ./dist/client-app.zip ]]
+then
+	rm ./dist/client-app.zip
+fi
+npm run build
+zip -r ./dist/client-app.zip  ./dist/
